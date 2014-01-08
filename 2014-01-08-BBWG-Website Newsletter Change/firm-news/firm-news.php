b51e2a0765a59f141b882656fac542ae83d01157
Gene Molchanov <gmolchanov@bermangrp.com>
2014-01-08 13:52:38 -0500

    added BBWG, NAIOP, NPCR projects

---
+++ 2014-01-08-BBWG-Website Newsletter Change/firm-news/firm-news.php
@@ -0,0 +1,1 @@
+This is the firm news section
---
+++ 2014-01-08-BBWG-Website Newsletter Change/firm-news/firm-newsletter.php
@@ -0,0 +1,262 @@
+<ul class="tabs">
+	<li><a href="#2014">2014</a></li>
+    <li><a href="#2013">2013</a></li>
+	<li><a href="#2012">2012</a></li>
+	<li><a href="#2011">2011</a></li>
+	<li><a href="#2010">2010</a></li>
+	<li><a href="#2009">2009</a></li>
+	<li><a href="#2008">2008</a></li>
+</ul>
+
+<div class="panes">
+	
+    <div>
+    
+   
+    
+    
+       <h3><a href="http://issuu.com/bbwg/docs/bbwg_january_2014?e=6008057/6228802" target="_blank">January 2014</a></h3>
+		<ul>
+			<li>New Year Message</li>
+			<li>Owner Success at the Tenant Protection Unit</li>
+			<li>MCI Increases Above Standard Increases</li>
+            <li>The Due Diligence Process for a One to Four Family Home</li>
+            <li>Know Your Tenant</li>
+		</ul>
+    
+    </div>
+    
+    
+    <div>
+    
+		
+        
+        <h3><a href="http://issuu.com/bbwg/docs/bbwg_december_2013__3_?e=6008057/5938733" target="_blank">December 2013</a></h3>
+		<ul>
+			<li>The New Loft Law Regulations</li>
+			<li>The Importance of Applying to Restore Rent</li>
+			<li>Knowledge is Power: The Importance of Due Diligence</li>
+		</ul>
+        
+        
+        
+        
+		<h3><a href="http://issuu.com/bbwg/docs/bbwg_november_2013__10_?e=6008057/5527453" target="_blank">November 2013</a></h3>
+		<ul>
+			<li>Defer Now..Pay Later..The Ins and Outs of 1031 Exchanges</li>
+			<li>Say Uncle</li>
+			<li>Recovery Of Possession After Foreclosure</li>
+		</ul>
+		<h3><a href="http://issuu.com/bbwg/docs/bbwg_october_2013?e=6008057/5101269" target="_blank">October 2013</a></h3>
+		<ul>
+			<li>The Path to City Hall and Rent Regulation</li>
+			<li>Chronic Non-Payment of Rent in Commercial Proceedings</li>
+			<li>What to do About Offensive Odors in Apartment Buildings</li>
+			<li>Mortgagors and Bankruptcy – Part 2: What Happens When a Mortgagor Lists a Pre-Peteition Claim Against His Lender in a Subsequent Bankruptcy Filing</li>
+			<li>Appellate Court Rejects DHCR’s Attempt to Alter One of its Longstanding Policies Relating to the Processing of MCI Applications</li>
+		</ul>
+		<h3><a href="http://issuu.com/bbwg/docs/bbwg_september_2013_081213__6_?e=6008057/4405245" target="_blank">September 2013</a></h3>
+		<ul>
+			<li>Remedies for “Bad Faith” During Mandatory Settlement Conferences in Foreclosure Actions</li>
+			<li>Sixty Day Rider is Valid to Terminate a Rent Stabilized Lease</li>
+			<li>Commercial Lease Guaranty Must Specify Guarantor’s Post-Lease Expiration Obligations</li>
+			<li>Trust-Owned Apartments Could be Eligible Again for Co-Op/Condo Tax Abatement</li>
+			<li>Contracts Required for MCI Applications</li>
+		</ul>
+		<h3><a href="../../media/BBWG_May 2013_5_web.pdf" target="_blank">May/June 2013</a></h3>
+		<ul>
+			<li>Congratulations to BBWG Super Lawyers</li>
+			<li>Preserving Landlord’s Claim for Arrears Against an Estate</li>
+			<li>When Does Permanent Actually Mean Permanent (Cacatur)</li>
+			<li>BBWG is Blogging</li>
+			<li>BBWG in the News</li>
+			<li>BBWG as Industry Speakers</li>
+		</ul>
+		<h3><a href="../../media/BBWG_March2013_web.pdf" target="_blank">March/April 2013</a></h3>
+		<ul>
+			<li>Party Walls: Two Walls for the Price of One</li>
+			<li>Co-Op/Condo Tax Abatement Reinstated (Mostly)</li>
+			<li>What Owners Need to Know About the Legalization Process Under the New Loft Law</li>
+			<li>It May Not Be Too Late</li>
+			<li>Deregulate Rent Regulated Apartments Through High Income High Rent Deregulation in 2013</li>
+		</ul>
+		<h3><a href="../../media/BBWG_January2013_web.pdf" target="_blank">January 2013</a></h3>
+		<ul>
+			<li>DHCR’s Attempt to Alter One of its Longstanding policies Relating to the processing of MCI Applications Has Been Rejected by the NYS Supreme Court</li>
+			<li>Commercial Landlords Beware: Gratuitous Space Used by a Tenant May Become a Rent-Free Addition to the Tenant’s Leased Premises</li>
+		</ul>
+	</div>
+	<div>
+		<h3><a href="http://www.bbwg.com/CM/Custom/2012-December-Newsletter.pdf" target="_blank">December 2012</a></h3>
+		<ul>
+			<li>Sandy's Aftermath: What Does It Mean For You, The Landlord?</li>
+			<li>Is There A Way To Evict A Tenant For Failure To Pay Rent Other Than The Nonpayment Proceeding?</li>
+			<li>Electronic Recording Comes To New York</li>
+			<li>Rent Deposits And RPAPL § 745(2)</li>
+		</ul>
+		<h3><a href="../../media/BBWG_November-Newsletter.pdf" target="_blank">November 2012</a></h3>
+		<ul>
+			<li>Negotiating a Tenant Release on Assignment of Lease: What to Look for From a Landlord’s Perspective</li>
+			<li>Liquidated Damages</li>
+			<li>Residential Tenants Liable for Future Rent</li>
+			<li>Major Capital Improvement Rent Increases: Recouping Costs of Ancillary Work</li>
+		</ul>
+		<h3><a href="http://www.bbwg.com/CM/Custom/BBWG_Newsletter_September_2012%20no%20crops.pdf" target="_blank">September 2012</a></h3>
+		<ul>
+			<li>No New Reason For Concern By Board Members</li>
+			<li>Heat And Hot Water Violations:Recent Changes To The Code</li>
+			<li>Dhcr Reverses Position On Mci Increase For Backflow Preventers</li>
+			<li>Should An Owner Enter Into A Stipulation Of Settlement Or Go To Trial In Non-Payment Housing Court Proceeding?</li>
+			<li>Tenant's Failure To Satisfy A Condition Precedent In A Commercial Lease Bars Tenant's $600,000 Overcharge Claim</li>
+		</ul>
+		<h3><a href="http://www.bbwg.com/CM/Custom/BBWG_Newsletter_June_2012.pdf" target="_blank">June 2012</a></h3>
+		<ul>
+			<li>Is The Deemed Lease Dead?</li>
+			<li>Additional Thoughts On The Deemed Lease Remedy</li>
+			<li>Practical Pointers On Improving Your Prospects At Ecb</li>
+			<li>Disability Compliance - When And When Not Required</li>
+		</ul>
+		<h3><a href="http://www.bbwg.com/CM/Newsletter/2012%20May%20Newsletter.pdf" target="_blank">May 2012</a></h3>
+		<ul>
+			<li>Congratulations To BBWG Super Lawyers</li>
+			<li>DHCR's Attempt to Alter One of Its Longstanding policies Relating to the processing of MCI Applications Is Rejected by the Court</li>
+			<li>Tenants' Cloaking of Their Vacatur Defeated a Daughter's Succession Claim</li>
+			<li>Logistics of Letters of Credit</li>
+		</ul>
+		<h3><a href="http://www.bbwg.com/CM/Newsletter/2012-March-Newsletter.pdf" target="_blank">March 2012</a></h3>
+		<ul>
+			<li>Cyber Sleuthing - Facebook Is Your Best FriendStipulations in Nonpayment Proceedings</li>
+			<li>Short-Term Rentals In New York City: If You See Something, Say Something</li>
+			<li>Appellate Division Restricts Succession Rights In Rent Stabilized Apartments</li>
+			<li>Final Standard Adjustment Factor for 2012-2013 MBRs</li>
+		</ul>
+		<h3><a href="http://www.bbwg.com/CM/Custom/February%202012%20Newsletter.pdf" target="_blank">February 2012</a></h3>
+		<ul>
+			<li>Harmon v. Kimmel Is Rent Stabilization Constitutional?</li>
+			<li>Stipulations in Nonpayment Proceedings</li>
+			<li>Mortgage Mash Up</li>
+			<li>DHCR Upholds Order of Deregulation Despite Existence of J-51 Tax Benefits</li>
+			<li>New Rules Concerning Fuel Cost Adjustments</li>
+		</ul>
+		<h3><a href="http://www.bbwg.com/CM/Custom/January%202012%20Newsletter.pdf" target="_blank">January 2012</a></h3>
+		<ul>
+			<li>Electronic Recording Comes to New York</li>
+			<li>BBWG Employs an Innovative Procedural Defense to Vacate a Fine Imposed on a Property Owner</li>
+			<li>Major Capital Improvement Rent Increases</li>
+			<li>Eviction Q &amp; A</li>
+		</ul>
+	</div>
+	<div>
+		<ul class="archive">
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/BBWG-December-2011.pdf" target="_blank" title="BBWG Newsletter December 2011">December 2011</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/BBWG-November-2011.pdf" target="_blank" title="BBWG Newsletter November 2011">November 2011</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/BBWG-Septmeber-2011.pdf" target="_blank" title="BBWG_Newsletter_September_2011">September 2011</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/BBWG-June-July-2011.pdf" target="_blank" title="BBWG_Newsletter_June-July_2011">June-July 2011</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Custom/BBWG_Newsletter_May_2011%20single.pdf" target="_blank" title="BBWG_Newsletter_May_2011 single">May 2011</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Custom/BBWG_Newsletter_April_2011%20single.pdf" target="_blank" title="BBWG_Newsletter_April_2011 single">April 2011</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/March-2011-Newsletter.pdf" target="_blank">March 2011</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/January-2011.pdf" target="_blank">January 2011</a>
+			</li>
+		</ul>
+	</div>
+	<div>
+		<ul class="archive">
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/dec-2010.pdf" target="_blank">December 2010</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/November-2010.pdf" target="_blank">November 2010</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/October-2010.pdf" target="_blank">October 2010</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/September-2010.pdf" target="_blank">September 2010</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/June.pdf" target="_blank">June 2010 </a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/April-May.pdf" target="_blank">April/May 2010</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/March.pdf" target="_blank">March 2010</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/Feb.pdf" target="_blank">February 2010</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/January-2010.pdf" target="_blank">January 2010</a>
+			</li>
+		</ul>
+	</div>
+	<div>
+		<ul class="archive">
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/December-2009.pdf" target="_blank" title="December 2009 Update">December 2009</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Custom/November2009Update.pdf" target="_blank" title="November 2009 Update">November 2009</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Custom/Sep-2009-update.pdf" target="_blank" title="September 2009 Update">September 2009 </a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Custom/June2009Update.pdf" target="_blank" title="June 2009 Update">June 2009 </a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Custom/April2009Update.pdf" target="_blank" title="April 2009 Update">April 2009</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Custom/March2009Update.pdf" target="_blank" title="March 2009 Update">March 2009</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/February2009Update.pdf" target="_blank">February 2009</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/January2009Update.pdf" target="_blank" title="January 2009 Update">January 2009</a>
+			</li>
+		</ul>
+	</div>
+	<div>
+		<ul class="archive">
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/December2008Update.pdf" target="_blank" title="December 2008 Update">December 2008</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/November2008Update.pdf" target="_blank" title="June 2008 Update">November 2008</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/October2008Update.pdf" target="_blank" title="June 2008 Update">October 2008</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/September2008Update.pdf" target="_blank" title="June 2008 Update">September 2008</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/June2008Update.pdf" target="_blank" title="June 2008 Update">June 2008</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/April2008Update.pdf" target="_blank" title="April 2008 Update">April 2008</a>
+			</li>
+			<li>
+				<a href="http://www.bbwg.com/CM/Newsletter/March2008Update.pdf" target="_blank" title="March 2008 Update">March 2008</a>
+			</li>
+		</ul>
+	</div>
+</div>
+
